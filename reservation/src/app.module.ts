74384f9812b88d52ecc52c5139abe62236426735
Jimmy <39483730+LeeJimmy1805@users.noreply.github.com>
2020-12-30 23:52:49 +0100

    update

--- reservation/src/app.module.ts
+++ reservation/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReservationModule } from './reservation/reservation.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ReservationModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost'),
+    ReservationModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- reservation/src/reservation/reservation.module.ts
+++ reservation/src/reservation/reservation.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReservationController } from './reservation.controller';
 import { ReservationService } from './reservation.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
+  imports: [MongooseModule.forRoot('mongodb://localhost:3000')],
   controllers: [ReservationController],
   providers: [ReservationService]
 })
